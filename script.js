696ad1f40e328633ff019fbf50ec280b32b36c94
Pema Lamu Sherpa <pemalhamusherpa555@gmail.com>
2025-05-15 10:31:54 +0545

    Add some projects

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            window.scrollTo({
-                top: target.offsetTop - 75,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
-
 // Active navigation link highlighting
 window.addEventListener('scroll', () => {
     const sections = document.querySelectorAll('section');
